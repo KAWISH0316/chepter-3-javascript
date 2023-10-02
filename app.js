ac81357ba188381cec201ef7753cd13071ac47d9
KAWISH0316 <kawishpc1122@gmail.com>
2023-10-02 14:46:53 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var age = "I am 22 years old"
+alert(age)
+
+var info = "you have visited this site 14 times"
+alert(info)
+
+var doc1 = "My Birth Year is 2000"
+var doc2 = "Data Type of my declared variable is number"
+
+document.write(doc1 +"<br/>"+ doc2)
+
+
+document.write("<h1>XYZ Cloth</h1>")
+document.write("<br/>John Doe Ordered d 5 T-shirt(s) on XYZ Clothing store")
+
+
+
